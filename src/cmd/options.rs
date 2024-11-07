594489609f4042ea5c717f5c499d86cefb115bbc
Badr Azizi <badrazizi3@gmail.com>
2024-11-07 16:49:50 +0300

    added a trait to output a pair of text and callback command

--- src/cmd/options.rs
+++ src/cmd/options.rs
@@ -1,25 +1,67 @@
-use strum_macros::{Display, EnumIter};
 use teloxide::utils::command::BotCommands;
 
-#[derive(BotCommands, Display)]
-#[command(rename_rule = "lowercase")]
-#[strum(serialize_all = "lowercase")]
+pub trait OptionsPair {
+    fn get_options() -> Vec<(String, String)>;
+}
+
+#[derive(BotCommands)]
+#[command(rename_rule = "PascalCase")]
 pub enum Command {
     Start,
 }
 
-#[derive(BotCommands, Display, EnumIter)]
-#[command(rename_rule = "lowercase")]
-#[strum(serialize_all = "lowercase")]
-pub enum Language {
-    العربية,
-    English,
+impl OptionsPair for Command {
+    fn get_options() -> Vec<(String, String)> {
+        vec![("أبداء".to_string(), "/Start".to_string())]
+    }
 }
 
-#[derive(BotCommands, Display, EnumIter)]
-#[command(rename_rule = "lowercase")]
-#[strum(serialize_all = "lowercase")]
-pub enum RuqyahArabic {
-    الرقية_الشرعية,
-    الرقية_من_العين_والحسد,
+#[derive(BotCommands)]
+#[command(rename_rule = "PascalCase")]
+pub enum Start {
+    Ruqyah,
+    AboutRuqyah,
+    RulingRuqyah,
+    VirtueOfRuqyah,
+    AboutBot,
+}
+
+impl OptionsPair for Start {
+    fn get_options() -> Vec<(String, String)> {
+        vec![
+            ("الرقية الشرعية".to_string(), "/Ruqyah".to_string()),
+            ("عن الرقية الشرعية".to_string(), "/AboutRuqyah".to_string()),
+            (
+                "حكم الرقية الشرعية".to_string(),
+                "/RulingRuqyah".to_string(),
+            ),
+            (
+                "فضل الرقية الشرعية".to_string(),
+                "/VirtueOfRuqyah".to_string(),
+            ),
+            ("عن البوت".to_string(), "/AboutBot".to_string()),
+        ]
+    }
+}
+
+#[derive(BotCommands)]
+#[command(rename_rule = "PascalCase")]
+pub enum Ruqyah {
+    RuqyahFromTheHolyQuran,
+    RuqyahFromTheNoblePropheticSunnah,
+}
+
+impl OptionsPair for Ruqyah {
+    fn get_options() -> Vec<(String, String)> {
+        vec![
+            (
+                "الرقية الشرعية من القرآن الكريم".to_string(),
+                "/RuqyahFromTheHolyQuran".to_string(),
+            ),
+            (
+                "الرقية الشرعية من السنة النبوية الشريفة".to_string(),
+                "/RuqyahFromTheNoblePropheticSunnah".to_string(),
+            ),
+        ]
+    }
 }
