6908fe693fac493bd65b252a47a4038afde2ebe3
Badr Azizi <badrazizi3@gmail.com>
2024-11-09 18:00:03 +0300

    removed build script

--- build.rs
+++
@@ -1,9 +0,0 @@
-extern crate dotenv;
-
-fn main() {
-    let dotenv_path = dotenv::dotenv().expect("failed to find .env file");
-    println!("cargo:rerun-if-changed={}", dotenv_path.display());
-    for (key, value) in dotenv::vars() {
-        println!("cargo:rustc-env={key}={value}");
-    }
-}
