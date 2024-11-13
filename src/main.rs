b229b04ecbcf0def0fc7fd3ed574efe772016d23
Badr Azizi <badrazizi3@gmail.com>
2024-11-13 21:56:41 +0300

    updated main.rs - change main derive - moved start bot to it own method - added a method to watch for options.json file changes

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
-use cmd::{callback, command, db};
-use std::env;
-use teloxide::{dispatching::UpdateFilterExt, dptree, prelude::Dispatcher, types::Update, Bot};
+use cmd::{callback, command, json::JSON};
+use config::file::Configurations;
+use database::{table::Table, users::Users};
+use notify::{Event, RecursiveMode, Watcher};
+use rusqlite::Connection;
+use std::{path::Path, sync::mpsc, time::Duration};
+use teloxide::{
+    dispatching::UpdateFilterExt,
+    dptree,
+    prelude::Dispatcher,
+    types::{CallbackQuery, Me, Message, Update},
+    Bot,
+};
+use tokio::{task, time};
 mod cmd;
+mod config;
+mod database;
 
-#[cfg(not(target_os = "windows"))]
+#[cfg(not(debug_assertions))]
 #[tokio::main]
 async fn main() {
     let _ = init().await;
 }
 
-#[cfg(target_os = "windows")]
+#[cfg(debug_assertions)]
 fn main() {
     let child = std::thread::Builder::new()
         .stack_size(4096 * 1024)
@@ -27,13 +40,36 @@ fn main() {
 }
 
 async fn init() -> Result<(), Box<dyn std::error::Error>> {
-    let _ = db::init_tables();
+    init_tables()?;
 
-    let bot = Bot::new(env!("TELEGRAM_BOT_TOKEN"));
+    JSON::init_json().await;
+
+    task::spawn(file_changed_notify());
+
+    let config = Configurations::get_config();
+
+    if !config.telegram_token.is_empty() {
+        start_bot(config.telegram_token.clone()).await?;
+    } else {
+        panic!("telegram bot token not set");
+    }
+
+    Ok(())
+}
+
+async fn start_bot(token: String) -> Result<(), Box<dyn std::error::Error>> {
+    let bot = Bot::new(token);
+
     // Create a handler for our bot, that will process updates from Telegram
     let handler = dptree::entry()
-        .branch(Update::filter_message().endpoint(command::handler))
-        .branch(Update::filter_callback_query().endpoint(callback::handler));
+        .branch(
+            Update::filter_message().endpoint(|bot: Bot, msg: Message, me: Me| async move {
+                command::handler(bot, msg, me).await
+            }),
+        )
+        .branch(Update::filter_callback_query().endpoint(
+            |bot: Bot, q: CallbackQuery, me: Me| async move { callback::handler(bot, q, me).await },
+        ));
 
     Dispatcher::builder(bot, handler)
         .enable_ctrlc_handler()
@@ -42,4 +78,54 @@ async fn init() -> Result<(), Box<dyn std::error::Error>> {
         .await;
 
     Ok(())
+}
+
+fn init_tables() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Database opened successfully");
+    let connection: Connection = match Connection::open("./database.db") {
+        Ok(connection) => connection,
+        Err(_) => {
+            return Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Error while opening database",
+            )));
+        }
+    };
+
+    match Users::init_table(&connection) {
+        Ok(_) => {}
+        Err(_) => {
+            return Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Error while creating Users table",
+            )));
+        }
+    }
+
+    Ok(())
+}
+
+async fn file_changed_notify() {
+    let (tx, rx) = mpsc::channel::<notify::Result<Event>>();
+
+    if let Ok(mut watcher) = notify::recommended_watcher(tx) {
+        if let Ok(_) = watcher.watch(Path::new("."), RecursiveMode::NonRecursive) {
+            for res in rx {
+                if let Ok(event) = res {
+                    if event.kind.is_modify() {
+                        for path in event.paths.iter() {
+                            if path.is_file() {
+                                if let Some(file_name) = path.file_name() {
+                                    if file_name == "options.json" {
+                                        time::sleep(Duration::from_millis(2000)).await;
+                                        JSON::init_json().await;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
